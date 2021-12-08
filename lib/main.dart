86176309b57925ccbc980e421e690fc043d0009a
Girolamo Giordano <56109364+girolamo-giordano@users.noreply.github.com>
2021-12-08 18:13:54 +0100

    commit iniziale

--- lib/main.dart
+++ lib/main.dart
@@ -96,7 +96,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times app challenge:',
+              'You have pushed the button this many times app challenge emad:',
             ),
             Text(
               '$_counter',
